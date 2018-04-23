d813035cf66acf11ef93aa3619269195a0a8d759
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-04-23 09:47:31 +0530

    displayed data from posts table

--- index.php
+++ index.php
@@ -12,6 +12,25 @@
             <!-- Blog Entries Column -->
             <div class="col-md-8">
 
+            <?php 
+            
+                $query = "SELECT * FROM posts";
+                $result = mysqli_query($connection, $query);
+
+                while($row = mysqli_fetch_array($result))
+                {
+                    $post_id            = $row['post_id'];
+                    $post_category_id   = $row['post_category_id'];
+                    $post_title         = $row['post_title'];
+                    $post_author        = $row['post_author'];
+                    $post_date          = $row['post_date'];
+                    $post_image         = $row['post_image'];
+                    $post_content       = $row['post_content'];
+                    $post_tags          = $row['post_tags'];
+                    $post_comment_count = $row['post_comment_count'];
+                    $post_status        = $row['post_status'];
+            ?>
+
                 <h1 class="page-header">
                     Page Heading
                     <small>Secondary Text</small>
@@ -19,19 +38,27 @@
 
                 <!-- First Blog Post -->
                 <h2>
-                    <a href="#">Blog Post Title</a>
+                    <a href="#"><?php echo $post_title; ?></a>
                 </h2>
                 <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
+                    by <a href="index.php"><?php echo $post_author; ?></a>
                 </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
+                <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date; ?></p>
                 <hr>
                 <img class="img-responsive" src="http://placehold.it/900x300" alt="">
                 <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
+                <p><?php echo $post_content; ?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                 <hr>
+
+            <?php
+                }
+            ?>
+
+                
+
+                
 
             </div>
 
