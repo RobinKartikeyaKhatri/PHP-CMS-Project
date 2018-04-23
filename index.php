a7ce7c2cb53d54275d1afdae85f9fe0ec5ca6aae
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-04-23 10:18:05 +0530

    displaying post_images in index.php file

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date; ?></p>
                 <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
                 <hr>
                 <p><?php echo $post_content; ?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
