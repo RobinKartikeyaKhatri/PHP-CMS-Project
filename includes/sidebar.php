fc12afbd80880f06e1dea8422050ad2c579302bc
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-04-23 11:28:52 +0530

    added search.php in form action attribute

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -3,15 +3,17 @@
                 <!-- Blog Search Well -->
                 <div class="well">
                     <h4>Blog Search</h4>
-                    <div class="input-group">
-                        <input type="text" class="form-control">
-                        <span class="input-group-btn">
-                            <button class="btn btn-default" type="button">
-                                <span class="glyphicon glyphicon-search"></span>
-                        </button>
-                        </span>
-                    </div>
-                    <!-- /.input-group -->
+                    <form action="search.php" method="post">
+                        <div class="input-group">
+                            <input type="text" name="search" class="form-control">
+                                <span class="input-group-btn">
+                                    <button name="submit" class="btn btn-default" type="submit">
+                                        <span class="glyphicon glyphicon-search"></span>
+                                    </button>
+                                </span>
+                        </div>
+                        <!-- /.input-group -->
+                    </form>
                 </div>
 
                 <!-- Blog Categories Well -->
