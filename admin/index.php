852e92b92189bf86f57120de519a06971fc6456a
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-04-23 14:33:32 +0530

    added a link to return to home site

--- admin/index.php
+++ admin/index.php
@@ -47,6 +47,7 @@
             </div>
             <!-- Top Menu Items -->
             <ul class="nav navbar-right top-nav">
+                <li><a href="../index.php">Home Site</a></li>
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-envelope"></i> <b class="caret"></b></a>
                     <ul class="dropdown-menu message-dropdown">
