903aa12379828fc6c5713426b570d84e23c74505
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-04-23 15:38:53 +0530

    added a form to add categories

---
+++ admin/categories.php
@@ -0,0 +1,44 @@
+<?php include("includes/header.php"); ?>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <?php include("includes/navigation.php"); ?>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Welcome To Admin
+                            <small>Author Name Here</small>
+                        </h1>
+                        <div class="col-xs-6">
+                            <form action="" method="post">
+                                <div class="form-group">
+                                    <label for="cat_title">Add Category</label>
+                                    <input type="text" name="cat_title" class="form-control" placeholder="Category">
+                                </div>
+                                <div class="form-group">
+                                    <input type="submit" name="submit" class="btn btn-primary" value="Add Category">
+                                </div>
+                            </form>
+                        </div>
+                        
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+<?php include("includes/footer.php"); ?>
