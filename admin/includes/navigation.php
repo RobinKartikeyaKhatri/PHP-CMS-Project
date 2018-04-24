ffb13bc4b4ad539c464c9e8a6e6a2024358a5ebe
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-04-24 13:02:38 +0530

    created links for sidebar in admin area

--- admin/includes/navigation.php
+++ admin/includes/navigation.php
@@ -37,7 +37,7 @@
                         <a href="javascript:;" data-toggle="collapse" data-target="#posts_dropdown"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
                         <ul id="posts_dropdown" class="collapse">
                             <li>
-                                <a href="#">View All Posts</a>
+                                <a href="./posts.php">View All Posts</a>
                             </li>
                             <li>
                                 <a href="#">Add Posts</a>
@@ -46,11 +46,11 @@
                     </li>
 
                     <li>
-                        <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Categories</a>
+                        <a href="./categories.php"><i class="fa fa-fw fa-wrench"></i> Categories</a>
                     </li>
 
                     <li class="active">
-                        <a href="blank-page.html"><i class="fa fa-fw fa-file"></i> Comments</a>
+                        <a href="./comments.php"><i class="fa fa-fw fa-file"></i> Comments</a>
                     </li>
                     <li>
                         <a href="javascript:;" data-toggle="collapse" data-target="#users"><i class="fa fa-fw fa-arrows-v"></i> Users <i class="fa fa-fw fa-caret-down"></i></a>
@@ -64,7 +64,7 @@
                         </ul>
                     </li>
                     <li>
-                        <a href="blank-page.html"><i class="fa fa-fw fa-file"></i> Profile</a>
+                        <a href="./profile.php"><i class="fa fa-fw fa-file"></i> Profile</a>
                     </li>
                 </ul>
             </div>
