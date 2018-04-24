c9bdf6f1bc92555b12717e8ce219a7627ed098cd
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-04-24 14:14:34 +0530

    placed all the code related to displaying posts data

---
+++ admin/includes/view_all_posts.php
@@ -0,0 +1,54 @@
+<table class="table table-responsive table-hover">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Author</th>
+            <th>Title</th>
+            <th>Category</th>
+            <th>Status</th>
+            <th>Image</th>
+            <th>Tags</th>
+            <th>Comments</th>
+            <th>Date</th>
+        </tr>
+    </thead>
+    <tbody>
+
+    <?php //Showing all posts
+
+        $query = "SELECT * FROM posts";
+        $result = mysqli_query($connection, $query);
+
+        while ($row = mysqli_fetch_array($result)) 
+        {
+            $post_id = $row['post_id'];
+            $post_category_id = $row['post_category_id'];
+            $post_title = $row['post_title'];
+            $post_author = $row['post_author'];
+            $post_date = $row['post_date'];
+            $post_image = $row['post_image'];
+            $post_content = $row['post_content'];
+            $post_tags = $row['post_tags'];
+            $post_comment_count = $row['post_comment_count'];
+            $post_status = $row['post_status'];
+    ?>
+
+            <tr>
+            <td><?php echo $post_id; ?></td>
+            <td><?php echo $post_author; ?></td>
+            <td><?php echo $post_title; ?></td>
+            <td><?php echo $post_category_id; ?></td>
+            <td><?php echo $post_status; ?></td>
+            <td><img class='img-responsive' width='100' src='../images/<?php echo $post_image; ?>'></td>
+            <td><?php echo $post_tags; ?></td>
+            <td><?php echo $post_comment_count; ?></td>
+            <td><?php echo $post_date; ?></td>
+        </tr>
+    <?php    
+        }
+
+    ?>
+
+        
+    </tbody>
+</table>
