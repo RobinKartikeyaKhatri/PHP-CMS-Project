00305887540105b995c2282a1533e6f7bf0bb1a7
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-04-24 09:21:22 +0530

    indented code in update_categories.php file

--- admin/includes/update_categories.php
+++ admin/includes/update_categories.php
@@ -1,51 +1,48 @@
 <!-- Form for Update Categories -->
 <form action="" method="post">
-                                <div class="form-group">
-                                    <label for="cat_title">Edit Category</label>
+    <div class="form-group">
+        <label for="cat_title">Edit Category</label>
 
-                                    <?php
-                                        // Catching GET request for updating categories
-                                        if (isset($_GET['edit'])) 
-                                        {
-                                            $the_edit_id = $_GET['edit'];
+        <?php
+        // Catching GET request for updating categories
+            if (isset($_GET['edit'])) 
+            {
+                $the_edit_id = $_GET['edit'];
 
-                                            $query = "SELECT * FROM categories WHERE cat_id = {$the_edit_id}";
-                                            $result = mysqli_query($connection, $query);
+                $query = "SELECT * FROM categories WHERE cat_id = {$the_edit_id}";
+                $result = mysqli_query($connection, $query);
 
-                                            while ($row = mysqli_fetch_array($result)) 
-                                            {
-                                                $cat_id = $row['cat_id'];
-                                                $cat_title = $row['cat_title'];
-                                    ?>
+                while ($row = mysqli_fetch_array($result)) 
+                {
+                    $cat_id = $row['cat_id'];
+                    $cat_title = $row['cat_title'];
+        ?>
 
-                                    <input type="text" name="cat_title" value="<?php  if(isset($cat_title)) {echo $cat_title;}?>" class="form-control" placeholder="Category">
+        <input type="text" name="cat_title" value="<?php  if(isset($cat_title)) {echo $cat_title;}?>" class="form-control" placeholder="Category">
 
-                                    <?php   
+        <?php                                   
+                }
+            }
+        ?>
+
+        <?php
+        // Code for Updating Categories
+            if(isset($_POST['update']))
+            {
+                $cat_title = $_POST['cat_title'];
+
+                $query = "UPDATE categories SET cat_title = '{$cat_title}' WHERE cat_id = $cat_id";
+                $result = mysqli_query($connection, $query);
+
+                if(!$result)
+                {
+                    die("Query failed " . mysqli_error($connection));
+                }
+            }
                                     
-                                            }
-                                        }
-                                    ?>
-
-                                    <?php
-                                        // Code for Updating Categories
-                                        if(isset($_POST['update']))
-                                        {
-                                            $cat_title = $_POST['cat_title'];
-
-                                            $query = "UPDATE categories SET cat_title = '{$cat_title}' WHERE cat_id = $cat_id";
-                                            $result = mysqli_query($connection, $query);
-
-                                            if(!$result)
-                                            {
-                                                die("Query failed " . mysqli_error($connection));
-                                            }
-                                        }
-                                    
-                                    ?>
-
-                                    
-                                </div>
-                                <div class="form-group">
-                                    <input type="submit" name="update" class="btn btn-warning" value="Update Category">
-                                </div>
-                            </form>
+        ?>                                
+    </div>
+    <div class="form-group">
+        <input type="submit" name="update" class="btn btn-warning" value="Update Category">
+    </div>
+</form>
