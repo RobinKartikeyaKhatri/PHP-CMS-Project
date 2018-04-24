c4713e49116678451c5d10787100fc1c991ac027
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-04-24 13:03:44 +0530

    created a table for displaying posts realted data

---
+++ admin/posts.php
@@ -0,0 +1,60 @@
+<?php include("includes/header.php"); ?>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <?php include("includes/navigation.php"); ?>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Welcome To Admin
+                            <small>Author Name Here</small>
+                        </h1>
+                        <table class="table table-responsive table-hover">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Author</th>
+                                    <th>Title</th>
+                                    <th>Category</th>
+                                    <th>Status</th>
+                                    <th>Image</th>
+                                    <th>Tags</th>
+                                    <th>Comments</th>
+                                    <th>Date</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>2</td>
+                                    <td>3</td>
+                                    <td>4</td>
+                                    <td>5</td>
+                                    <td>6</td>
+                                    <td>7</td>
+                                    <td>8</td>
+                                    <td>9</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+<?php include("includes/footer.php"); ?>
