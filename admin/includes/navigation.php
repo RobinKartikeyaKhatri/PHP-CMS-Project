ea267e26487c6a23458e23925ca43555c29c0fcd
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-04-25 17:19:17 +0530

    created a link to go to add_post.php page

--- admin/includes/navigation.php
+++ admin/includes/navigation.php
@@ -40,7 +40,7 @@
                                 <a href="./posts.php">View All Posts</a>
                             </li>
                             <li>
-                                <a href="#">Add Posts</a>
+                                <a href="./posts.php?source=add_post">Add Posts</a>
                             </li>
                         </ul>
                     </li>
