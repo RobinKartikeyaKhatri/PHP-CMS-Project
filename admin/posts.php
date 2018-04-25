c21b021da308252858a18b88785bb28fce098ec3
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-04-25 17:32:09 +0530

    created a case in switch statement to include edit_post.php file

--- admin/posts.php
+++ admin/posts.php
@@ -32,6 +32,10 @@
                                     include("includes/add_post.php");
                                     break;
                                 
+                                case 'edit_post':
+                                    include("includes/edit_post.php");
+                                    break;
+                                
                                 default:
                                     include("includes/view_all_posts.php");
                                     break;
