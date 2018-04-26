0c4c6491fa1ff06717a840b8184a4985bbac4a92
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-04-26 11:53:41 +0530

    used substr() function on post_content variable

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
                     $post_author        = $row['post_author'];
                     $post_date          = $row['post_date'];
                     $post_image         = $row['post_image'];
-                    $post_content       = $row['post_content'];
+                    $post_content       = substr($row['post_content'], 0, 50);
                     $post_tags          = $row['post_tags'];
                     $post_comment_count = $row['post_comment_count'];
                     $post_status        = $row['post_status'];
