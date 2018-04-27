1a4bd04e1dfacd33729ddc7b72435dbc32f6d8a1
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-04-27 13:39:03 +0530

    created comment form in post.php file

--- post.php
+++ post.php
@@ -66,11 +66,23 @@
                 <!-- Comments Form -->
                 <div class="well">
                     <h4>Leave a Comment:</h4>
-                    <form role="form">
+                    <form action="" method="post" role="form">
                         <div class="form-group">
-                            <textarea class="form-control" rows="3"></textarea>
+                            <label for="comment_author">Author</label>
+                            <input type="text" name="comment_author" class="form-control">
                         </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
+
+                        <div class="form-group">
+                            <label for="comment_email">Email</label>
+                            <input type="email" name="comment_email" class="form-control">
+                        </div>
+                        
+                        <div class="form-group">
+                            <label for="comment_content">Content</label>
+                            <textarea name="comment_content" class="form-control" rows="3"></textarea>
+                        </div>
+
+                        <button name="create_comment" type="submit" class="btn btn-primary">Submit</button>
                     </form>
                 </div>
 
