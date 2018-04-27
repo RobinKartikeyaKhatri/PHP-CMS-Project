b7a484ba97df99a023e9a8d928df75dc6f46ffaf
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-04-27 10:32:50 +0530

    displayed comments data in view_all_comments.php

--- admin/includes/view_all_comments.php
+++ admin/includes/view_all_comments.php
@@ -1,16 +1,15 @@
-<table class="table table-responsive table-hover">
+<table class="table table-responsive table-hover table-bordered">
     <thead>
         <tr>
             <th>ID</th>
             <th>Author</th>
-            <th>Title</th>
-            <th>Category</th>
+            <th>Comment</th>
+            <th>Email</th>
             <th>Status</th>
-            <th>Image</th>
-            <th>Tags</th>
-            <th>Comments</th>
+            <th>In Response To</th>
             <th>Date</th>
-            <th>Edit</th>
+            <th>Approve</th>
+            <th>Unapprove</th>
             <th>Delete</th>
         </tr>
     </thead>
@@ -18,49 +17,47 @@
 
     <?php //Showing all posts
 
-        $query = "SELECT * FROM posts";
+        $query = "SELECT * FROM comments";
         $result = mysqli_query($connection, $query);
 
         while ($row = mysqli_fetch_array($result)) 
         {
-            $post_id = $row['post_id'];
-            $post_category_id = $row['post_category_id'];
-            $post_title = $row['post_title'];
-            $post_author = $row['post_author'];
-            $post_date = $row['post_date'];
-            $post_image = $row['post_image'];
-            $post_content = $row['post_content'];
-            $post_tags = $row['post_tags'];
-            $post_comment_count = $row['post_comment_count'];
-            $post_status = $row['post_status'];
+            $comment_id         = $row['comment_id'];
+            $comment_post_id    = $row['comment_post_id'];
+            $comment_author     = $row['comment_author'];
+            $comment_email      = $row['comment_email'];
+            $comment_content    = $row['comment_content'];
+            $comment_status     = $row['comment_status'];
+            $comment_date       = $row['comment_date'];
+            
 
             echo "<tr>";
-            echo "<td>$post_id</td>";
-            echo "<td>$post_author</td>";
-            echo "<td>$post_title</td>";
+            echo "<td>$comment_id</td>";
+            echo "<td>$comment_author</td>";
+            echo "<td>$comment_content</td>";
 
-            $query = "SELECT * FROM categories WHERE cat_id = $post_category_id";
-            $select_categories = mysqli_query($connection, $query);
-            while($row = mysqli_fetch_array($select_categories))
-            {
-                $cat_id = $row['cat_id'];
-                $cat_title = $row['cat_title'];
+            // $query = "SELECT * FROM categories WHERE cat_id = $post_category_id";
+            // $select_categories = mysqli_query($connection, $query);
+            // while($row = mysqli_fetch_array($select_categories))
+            // {
+            //     $cat_id = $row['cat_id'];
+            //     $cat_title = $row['cat_title'];
 
-                echo "<td>$cat_title</td>";
-            }
+            //     echo "<td>$cat_title</td>";
+            // }
             
 
             
 
 
 
-            echo "<td>$post_status";
-            echo "<td><img class='img-responsive' width='100' src='../images/$post_image'</td>";
-            echo "<td>$post_tags</td>";
-            echo "<td>$post_comment_count</td>";
-            echo "<td>$post_date</td>";
-            echo "<td><a class='btn btn-warning' href='posts.php?source=edit_post&p_id=$post_id'>Edit</a></td>";
-            echo "<td><a class='btn btn-danger' href='posts.php?delete=$post_id'>Delete</a></td>";
+            echo "<td>$comment_email";
+            echo "<td>$comment_status</td>";
+            echo "<td>SOME TITLE</td>";
+            echo "<td>$comment_date</td>";
+            echo "<td><a class='btn btn-success' href='#'>Approve</a></td>";
+            echo "<td><a class='btn btn-warning' href='#'>Unapprove</a></td>";
+            echo "<td><a class='btn btn-danger' href='#'>Delete</a></td>";
             echo "</tr>";
         }
     ?>
