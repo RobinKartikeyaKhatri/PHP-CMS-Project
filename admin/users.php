50eb57793fc92890a76d37bd28568a6e8a6bfd4f
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-04-28 10:01:31 +0530

    created users.php page

---
+++ admin/users.php
@@ -0,0 +1,58 @@
+<?php include("includes/header.php"); ?>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <?php include("includes/navigation.php"); ?>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Welcome To Admin
+                            <small>Author Name Here</small>
+                        </h1>
+                        <?php
+                            // Displaying pages based on condition
+                            if (isset($_GET['source'])) 
+                            {
+                                $source = $_GET['source'];
+                            }
+                            else
+                            {
+                                $source = "";
+                            }
+
+                            switch ($source) {
+                                case 'add_user':
+                                    include("includes/add_user.php");
+                                    break;
+                                
+                                case 'edit_user':
+                                    include("includes/edit_user.php");
+                                    break;
+                                
+                                default:
+                                    include("includes/view_all_users.php");
+                                    break;
+                            }
+
+                        ?>
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+<?php include("includes/footer.php"); ?>
