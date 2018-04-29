88bf891109c2435e19c0598a56279622110aaa7c
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-04-29 13:25:26 +0530

    created query to display user data

--- admin/includes/view_all_users.php
+++ admin/includes/view_all_users.php
@@ -7,68 +7,53 @@
             <th>Lastname</th>
             <th>Email</th>
             <th>Role</th>
-            <th>Date</th>
         </tr>
     </thead>
     <tbody>
 
     <?php //Showing all posts
 
-        $query = "SELECT * FROM comments";
+        $query = "SELECT * FROM users";
         $result = mysqli_query($connection, $query);
 
         while ($row = mysqli_fetch_array($result)) 
         {
-            $comment_id         = $row['comment_id'];
-            $comment_post_id    = $row['comment_post_id'];
-            $comment_author     = $row['comment_author'];
-            $comment_email      = $row['comment_email'];
-            $comment_content    = $row['comment_content'];
-            $comment_status     = $row['comment_status'];
-            $comment_date       = $row['comment_date'];
+            $user_id            = $row['user_id'];
+            $username           = $row['username'];
+            $password           = $row['password'];
+            $user_firstname     = $row['user_firstname'];
+            $user_lastname      = $row['user_lastname'];
+            $user_email         = $row['user_email'];
+            $user_image         = $row['user_image'];
+            $user_role          = $row['user_role'];
+            
             
 
             echo "<tr>";
-            echo "<td>$comment_id</td>";
-            echo "<td>$comment_author</td>";
-            echo "<td>$comment_content</td>";
+            echo "<td>$user_id</td>";
+            echo "<td>$username</td>";
+            echo "<td>$user_firstname</td>";
+            echo "<td>$user_lastname</td>";
+            echo "<td>$user_email</td>";
 
-            // $query = "SELECT * FROM categories WHERE cat_id = $post_category_id";
-            // $select_categories = mysqli_query($connection, $query);
-            // while($row = mysqli_fetch_array($select_categories))
+            // $query = "SELECT * FROM posts WHERE post_id = $comment_post_id";
+            // $result_show = mysqli_query($connection, $query);
+
+            // while($row = mysqli_fetch_array($result_show))
             // {
-            //     $cat_id = $row['cat_id'];
-            //     $cat_title = $row['cat_title'];
+            //     $post_id = $row['post_id'];
+            //     $post_title = $row['post_title'];
 
-            //     echo "<td>$cat_title</td>";
+            //     echo "<td><a class='btn btn-primary' href='../post.php?p_id=$post_id'>$post_title</a></td>";
             // }
-            
 
             
 
 
-
-            echo "<td>$comment_email";
-            echo "<td>$comment_status</td>";
-
-            $query = "SELECT * FROM posts WHERE post_id = $comment_post_id";
-            $result_show = mysqli_query($connection, $query);
-
-            while($row = mysqli_fetch_array($result_show))
-            {
-                $post_id = $row['post_id'];
-                $post_title = $row['post_title'];
-
-                echo "<td><a class='btn btn-primary' href='../post.php?p_id=$post_id'>$post_title</a></td>";
-            }
-
-            
-
-
-            echo "<td>$comment_date</td>";
-            echo "<td><a class='btn btn-success' href='comments.php?approve=$comment_id'>Approve</a></td>";
-            echo "<td><a class='btn btn-warning' href='comments.php?unapprove=$comment_id'>Unapprove</a></td>";
-            echo "<td><a class='btn btn-danger' href='comments.php?delete=$comment_id'>Delete</a></td>";
+            echo "<td>$user_role</td>";
+            // echo "<td><a class='btn btn-success' href='comments.php?approve=$comment_id'>Approve</a></td>";
+            // echo "<td><a class='btn btn-warning' href='comments.php?unapprove=$comment_id'>Unapprove</a></td>";
+            // echo "<td><a class='btn btn-danger' href='comments.php?delete=$comment_id'>Delete</a></td>";
             echo "</tr>";
         }
     ?>
