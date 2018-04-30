9cd87f91637938fa70eac944d67e8426ab3a370b
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-04-30 10:09:53 +0530

    validating user in admin area, if the user_role is not 'Admin' than redirect him to home page

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -2,6 +2,19 @@
 <?php session_start(); ?>
 <?php include("../includes/db.php"); ?>
 <?php include("./functions.php"); ?>
+
+<?php
+
+if(isset($_SESSION['username']))
+{
+    if($_SESSION['user_role'] !== 'Admin')
+    {
+        header("Location: ../index.php");
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
