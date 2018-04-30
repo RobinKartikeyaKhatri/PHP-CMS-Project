49542a129b5258a963f8345a4fb047c7025ae950
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-04-30 08:45:13 +0530

    created a select query to select user data based upon login credentials

--- includes/login.php
+++ includes/login.php
@@ -1,4 +1,5 @@
 <?php ob_start(); ?>
+<?php session_start(); ?>
 <?php include("db.php"); ?>
 <?php
 
@@ -9,6 +10,22 @@ if(isset($_POST['login']))
 
    $query = "SELECT * FROM users WHERE username = '$username'";
    $result = mysqli_query($connection, $query);
+
+   if(!$result)
+   {
+        die("Query failed " . mysqli_error($connection));
+   }
+
+   while($row = mysqli_fetch_array($result))
+   {
+      $user_id          = $row['user_id'];
+      $username         = $row['username'];
+      $password         = $row['password'];
+      $user_firstname   = $row['user_firstname'];
+      $user_lastname    = $row['user_lastname'];
+
+      
+   }
 
    
 
