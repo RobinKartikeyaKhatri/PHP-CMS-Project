870103346d0fea39527063fd09762f96e8c01ca3
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-04-30 11:40:48 +0530

    created code for logout user

---
+++ includes/logout.php
@@ -0,0 +1,18 @@
+<?php session_start(); ?>
+
+<?php
+
+
+       $_SESSION['user_id']         = null;
+       $_SESSION['username']        = null;
+       $_SESSION['password']        = null;
+       $_SESSION['user_firstname']  = null;
+       $_SESSION['user_lastname']   = null;
+       $_SESSION['user_role']       = null;
+
+       session_destroy(); 
+
+       header("Location: ../index.php");
+
+
+?>
