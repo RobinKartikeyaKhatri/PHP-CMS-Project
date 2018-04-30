5f553374f9df898e3a616151633cc0aa357b8f6f
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-04-30 08:02:42 +0530

    created a login form

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -16,6 +16,23 @@
                     </form>
                 </div>
 
+                <!-- Login Form -->
+                <div class="well">
+                    <h4>Login</h4>
+                    <form action="includes/login.php" method="post">
+                        <div class="form-group">
+                            <input type="text" name="username" class="form-control" placeholder="Username">
+                        </div>
+                        <div class="input-group">
+                            <input type="password" name="password" class="form-control" placeholder="Password">
+                            <span class="input-group-btn">
+                                <button class="btn btn-primary" type="submit" name="login">Login</button>
+                            </span>
+                        </div>
+                        <!-- /.input-group -->
+                    </form>
+                </div>
+
                 <!-- Blog Categories Well -->
                 <div class="well">
                     <h4>Blog Categories</h4>
