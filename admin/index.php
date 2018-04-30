3c4eaf8ccb3e7700872f6f4d7971b695d9ff1cab
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-04-30 09:27:53 +0530

    echoed the username of the loggin user with the help of $_SESSION super global array

--- admin/index.php
+++ admin/index.php
@@ -14,7 +14,7 @@
                     <div class="col-lg-12">
                         <h1 class="page-header">
                             Welcome To Admin
-                            <small>Author Name Here</small>
+                            <small><?php echo $_SESSION['username']; ?></small>
                         </h1>
                         
                     </div>
