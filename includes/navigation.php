fd7170136684112ab3aa5fbdabb6db8f0cb91751
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-05-01 19:26:04 +0530

    created code for adding post edit link in front end

--- includes/navigation.php
+++ includes/navigation.php
@@ -35,7 +35,23 @@
                     
                     <li>
                         <a href="admin">Admin</a>
-                    </li> -->
+                    </li> 
+
+                    <?php
+
+                        if(isset($_SESSION['user_role']))
+                        {
+                            if(isset($_GET['p_id']))
+                            {
+                                $the_post_id = $_GET['p_id'];
+                                echo "<li>
+                                        <a href='admin/posts.php?source=edit_post&p_id=$the_post_id'>Edit Post</a>
+                                     </li>";
+                            }
+                        }
+
+                    ?>
+
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
