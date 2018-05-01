3a991a18eedda79101e99a34f9ec7fae8dd9c840
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-05-01 13:31:48 +0530

    removed some unwanted data from Google Charts

--- admin/index.php
+++ admin/index.php
@@ -160,17 +160,22 @@
 
                         function drawChart() {
                             var data = google.visualization.arrayToDataTable([
-                            ['Year', 'Sales', 'Expenses', 'Profit'],
-                            ['2014', 1000, 400, 200],
-                            ['2015', 1170, 460, 250],
-                            ['2016', 660, 1120, 300],
-                            ['2017', 1030, 540, 350]
+                            ['Data', 'Count'],
+
+                            <?php
+                            
+                            
+                            
+                            ?>
+
+                            ['Posts', 1000],
+                            
                         ]);
 
                             var options = {
                                 chart: {
-                                    title: 'Company Performance',
-                                    subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+                                    title: '',
+                                    subtitle: '',
                                 }
                             };
 
@@ -180,7 +185,7 @@
                         }
                     </script>
 
-                    <div id="columnchart_material" style="width: 800px; height: 500px;"></div>
+                    <div id="columnchart_material" style="width: 'auto'; height: 500px;"></div>
 
 
                 </div> <!-- End of Google Charts -->
