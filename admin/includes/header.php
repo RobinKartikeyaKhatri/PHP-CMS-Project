536b272822b8bdc263741e4a05b2de9a0fb98ead
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-05-01 13:20:09 +0530

    added Google Charts JS file

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -56,6 +56,8 @@ else if(isset($_SESSION['user_role']) && $_SESSION['user_role'] == "Subscriber")
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+
 </head>
 
 <body>
