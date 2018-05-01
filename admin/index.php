7478b6558b61c8eed98004c89386cf0841485fce
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-05-01 13:20:46 +0530

    added code display Google Charts

--- admin/index.php
+++ admin/index.php
@@ -153,6 +153,38 @@
                     </div>
                 </div>
 
+                <div class="row"> <!-- Google Charts -->
+                    <script type="text/javascript">
+                        google.charts.load('current', {'packages':['bar']});
+                        google.charts.setOnLoadCallback(drawChart);
+
+                        function drawChart() {
+                            var data = google.visualization.arrayToDataTable([
+                            ['Year', 'Sales', 'Expenses', 'Profit'],
+                            ['2014', 1000, 400, 200],
+                            ['2015', 1170, 460, 250],
+                            ['2016', 660, 1120, 300],
+                            ['2017', 1030, 540, 350]
+                        ]);
+
+                            var options = {
+                                chart: {
+                                    title: 'Company Performance',
+                                    subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+                                }
+                            };
+
+                            var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+                            chart.draw(data, google.charts.Bar.convertOptions(options));
+                        }
+                    </script>
+
+                    <div id="columnchart_material" style="width: 800px; height: 500px;"></div>
+
+
+                </div> <!-- End of Google Charts -->
+
             </div>
             <!-- /.container-fluid -->
 
