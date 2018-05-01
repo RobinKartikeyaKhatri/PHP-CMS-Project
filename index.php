2badc542f42c56e565985d5716fb78d030da84a9
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-05-01 18:09:39 +0530

    added links to post image and read more button to got specific post in front end

--- index.php
+++ index.php
@@ -51,10 +51,12 @@
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date; ?></p>
                 <hr>
-                <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
+                <a href="post.php?p_id=<?php echo $post_id; ?>">
+                    <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
+                </a>
                 <hr>
                 <p><?php echo $post_content; ?></p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                 <hr>
 
