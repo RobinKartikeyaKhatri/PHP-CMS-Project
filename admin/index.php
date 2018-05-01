2f5331e318547ccc1e6ee75a68f4179f9f1f00e3
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-05-01 10:08:18 +0530

    displayed all dynamic data in widgets

--- admin/index.php
+++ admin/index.php
@@ -63,7 +63,17 @@
                                         <i class="fa fa-comments fa-5x"></i>
                                     </div>
                                     <div class="col-xs-9 text-right">
-                                    <div class='huge'>23</div>
+
+                                    <?php
+                                    
+                                        $query = "SELECT * FROM comments";
+                                        $select_all_comments = mysqli_query($connection, $query);
+                                        $comments_counts = mysqli_num_rows($select_all_comments);
+
+                                        echo "<div class='huge'>$comments_counts</div>";
+                                    
+                                    ?>
+
                                     <div>Comments</div>
                                     </div>
                                 </div>
@@ -85,7 +95,17 @@
                                         <i class="fa fa-user fa-5x"></i>
                                     </div>
                                     <div class="col-xs-9 text-right">
-                                    <div class='huge'>23</div>
+
+                                    <?php
+                                    
+                                        $query = "SELECT * FROM users";
+                                        $select_all_users = mysqli_query($connection, $query);
+                                        $users_counts = mysqli_num_rows($select_all_users);
+
+                                        echo "<div class='huge'>$users_counts</div>";
+                                    
+                                    ?>
+
                                         <div> Users</div>
                                     </div>
                                 </div>
@@ -107,7 +127,17 @@
                                         <i class="fa fa-list fa-5x"></i>
                                     </div>
                                     <div class="col-xs-9 text-right">
-                                        <div class='huge'>13</div>
+
+                                    <?php
+
+                                        $query = "SELECT * FROM categories";
+                                        $select_all_categories = mysqli_query($connection, $query);
+                                        $categories_counts = mysqli_num_rows($select_all_categories);
+
+                                        echo "<div class='huge'>$categories_counts</div>";
+                                    
+                                    ?>
+
                                         <div>Categories</div>
                                     </div>
                                 </div>
