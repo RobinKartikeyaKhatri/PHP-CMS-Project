510bf5d450f9de0ac59d1623bb029de255990edf
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-05-02 09:02:53 +0530

    created code for draft and delete options

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -12,7 +12,19 @@ if(isset($_POST['checkBoxArray']))
                 $query = "UPDATE posts set post_status = '$bulk_options' WHERE post_id = $postValueId"; 
                 $update_to_published_status = mysqli_query($connection, $query);
                 confirmQuery($update_to_published_status);
-                break;
+            break;
+
+            case 'draft':
+                $query = "UPDATE posts set post_status = '$bulk_options' WHERE post_id = $postValueId"; 
+                $update_to_draft_status = mysqli_query($connection, $query);
+                confirmQuery($update_to_draft_status);
+            break;
+
+            case 'delete':
+                $query = "DELETE FROM posts WHERE post_id = $postValueId";
+                $update_to_delete_status = mysqli_query($connection, $query);
+                confirmQuery($update_to_delete_status);
+            break;
             
             default:
                 # code...
