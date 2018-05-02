c5178994d884092598ee4e52d24be9a8f447616c
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-05-02 10:36:25 +0530

    added dynamic personalization to navigation.php

--- admin/includes/navigation.php
+++ admin/includes/navigation.php
@@ -14,7 +14,7 @@
                 <li><a href="../index.php">Home Site</a></li>
                 
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> John Smith <b class="caret"></b></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> <?php echo $_SESSION['username']; ?> <b class="caret"></b></a>
                     <ul class="dropdown-menu">
                         <li>
                             <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
