2e4d7486fd931164c6be232188b8fd9467640f95
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-05-02 10:14:35 +0530

    added link to view post from admin

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -65,6 +65,7 @@ if(isset($_POST['checkBoxArray']))
                 <th>Tags</th>
                 <th>Comments</th>
                 <th>Date</th>
+                <th>View Post</th>
                 <th>Edit</th>
                 <th>Delete</th>
             </tr>
@@ -117,6 +118,7 @@ if(isset($_POST['checkBoxArray']))
                 echo "<td>$post_tags</td>";
                 echo "<td>$post_comment_count</td>";
                 echo "<td>$post_date</td>";
+                echo "<td><a class='btn btn-primary' href='../post.php?p_id=$post_id'>View Post</a></td>";
                 echo "<td><a class='btn btn-warning' href='posts.php?source=edit_post&p_id=$post_id'>Edit</a></td>";
                 echo "<td><a class='btn btn-danger' href='posts.php?delete=$post_id'>Delete</a></td>";
                 echo "</tr>";
