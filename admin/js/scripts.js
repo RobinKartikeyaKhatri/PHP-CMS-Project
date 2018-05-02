20eb83d132582e7893d0f58c33852b2f850287b3
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-05-02 09:22:54 +0530

    added js code for select all checkboxes

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -1,1 +1,16 @@
 tinymce.init({ selector:'textarea' });
+
+$(document).ready(function(){
+    $('#selectAllBoxes').click(function(event){
+        if(this.checked) {
+                $('.checkBoxes').each(function(){
+                    this.checked = true;
+                });
+        }
+        else{
+            $('.checkBoxes').each(function(){
+                this.checked = false;
+            });
+        }
+    });
+});
