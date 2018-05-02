10fb75ebf39c46b3cc212704babedf2b22737f62
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-05-02 10:36:58 +0530

    added dynamic personaliation to index.php

--- admin/index.php
+++ admin/index.php
@@ -13,7 +13,7 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">
-                            Welcome To Admin
+                            Welcome To Dashboard
                             <small><?php echo $_SESSION['username']; ?></small>
                         </h1>
                         
