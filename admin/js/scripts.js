838aeee21c9f97951caa0abf6ea581a75b2e81db
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-05-04 15:39:00 +0530

    crearted JS code for animation

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -13,4 +13,11 @@ $(document).ready(function(){
             });
         }
     });
+
+var divBox = "<div id='load-screen'><div id='loading'></div></div>";
+$("body").prepend(divBox);
+$('#load-screen').delay(700).fadeOut(600, function(){
+    $(this).remove();
+});
+
 });
