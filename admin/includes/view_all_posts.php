c3bede3f875dd0068b12792c74375930ac12c046
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-05-04 15:18:48 +0530

    created code for cloning posts and displayed post by DESCENDING ORDER

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -25,6 +25,36 @@ if(isset($_POST['checkBoxArray']))
                 $update_to_delete_status = mysqli_query($connection, $query);
                 confirmQuery($update_to_delete_status);
             break;
+
+            case 'clone':
+                $query = "SELECT * FROM posts WHERE post_id = $postValueId";
+                $select_post_query = mysqli_query($connection, $query);
+
+                while($row = mysqli_fetch_array($select_post_query))
+                {
+                    $post_title = $row['post_title'];
+                    $post_category_id = $row['post_category_id'];
+                    $post_date = $row['post_date'];
+                    $post_author = $row['post_author'];
+                    $post_status = $row['post_status'];
+                    $post_image = $row['post_image'];
+                    $post_tags = $row['post_tags'];
+                    $post_content = $row['post_content'];
+                    $post_comment_count = $row['post_comment_count'];
+                }
+
+                $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content,
+                            post_tags, post_comment_count, post_status) VALUES($post_category_id, '$post_title', 
+                            '$post_author', now(), '$post_image', '$post_content', '$post_tags', 
+                            $post_comment_count, '$post_status')";
+                $copy_query = mysqli_query($connection, $query);
+
+                if(!$copy_query)
+                {
+                    die("Query failed " . mysqli_error($connection));
+                }
+
+                break;
             
             default:
                 # code...
@@ -45,6 +75,7 @@ if(isset($_POST['checkBoxArray']))
             <option value="published">Publish</option>
             <option value="draft">Draft</option>
             <option value="delete">Delete</option>
+            <option value="clone">Clone</option>
         </select>
     </div>
 
@@ -74,7 +105,7 @@ if(isset($_POST['checkBoxArray']))
 
         <?php //Showing all posts
 
-            $query = "SELECT * FROM posts";
+            $query = "SELECT * FROM posts ORDER BY post_id DESC";
             $result = mysqli_query($connection, $query);
 
             while ($row = mysqli_fetch_array($result)) 
