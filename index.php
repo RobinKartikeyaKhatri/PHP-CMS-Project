8bb4c028fedf7f981df4df8fc76e53a7e50739af
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2018-05-04 14:50:02 +0530

    created a link to autor_posts.php page and send parameters

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
                     <a href="post.php?p_id=<?php echo $post_id;?>"><?php echo $post_title; ?></a>
                 </h2>
                 <p class="lead">
-                    by <a href="index.php"><?php echo $post_author; ?></a>
+                    by <a href="author_posts.php?author=<?php echo $post_author; ?>&p_id=<?php echo $post_id;?>"><?php echo $post_author; ?></a>
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date; ?></p>
                 <hr>
