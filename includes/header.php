ae2da259f2cd332e0319f7585706552f20b7eaf0
LalitKhatri <kartikrobin@gmail.com>
2018-05-05 08:42:29 +0530

    linked styles.css file

--- includes/header.php
+++ includes/header.php
@@ -18,6 +18,8 @@
     <!-- Custom CSS -->
     <link href="css/blog-home.css" rel="stylesheet">
 
+    <link href="css/styles.css" rel="stylesheet">
+
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
