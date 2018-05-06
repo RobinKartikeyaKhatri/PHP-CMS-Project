28e916f4594e72b0b494a03315d2a6acf10a618b
LalitKhatri <kartikrobin@gmail.com>
2018-05-06 16:17:06 +0530

    created code for instant users online count without refreshing browser

--- admin/functions.php
+++ admin/functions.php
@@ -74,29 +74,40 @@
 
     function user_online()
     {
-        global $connection;
-
-        $session = session_id();
-        $time = time();
-        $time_out_in_seconds = 60;
-        $time_out = $time - $time_out_in_seconds;
-
-        $query = "SELECT * FROM users_online WHERE session = '$session'";
-        $send_query = mysqli_query($connection, $query);
-
-        $count = mysqli_num_rows($send_query);
-
-        if($count == NULL)
+        if(isset($_GET['onlineusers']))
         {
-            mysqli_query($connection, "INSERT INTO users_online(session, time) VALUES('$session', '$time')");
-        }
-        else
-        {
-            mysqli_query($connection, "UPDATE users_online SET time = '$time' WHERE session = '$session'");
-        }
+            global $connection;
 
-        $users_online_query = mysqli_query($connection, "SELECT * FROM users_online WHERE time > '$time_out'");
-        return $count_user = mysqli_num_rows($users_online_query);
+            if(!$connection)
+            {
+                session_start();
+                include("../includes/db.php");
+
+                $session = session_id();
+                $time = time();
+                $time_out_in_seconds = 05;
+                $time_out = $time - $time_out_in_seconds;
+    
+                $query = "SELECT * FROM users_online WHERE session = '$session'";
+                $send_query = mysqli_query($connection, $query);
+    
+                $count = mysqli_num_rows($send_query);
+    
+                if($count == NULL)
+                {
+                    mysqli_query($connection, "INSERT INTO users_online(session, time) VALUES('$session', '$time')");
+                }
+                else
+                {
+                    mysqli_query($connection, "UPDATE users_online SET time = '$time' WHERE session = '$session'");
+                }
+    
+                $users_online_query = mysqli_query($connection, "SELECT * FROM users_online WHERE time > '$time_out'");
+                echo $count_user = mysqli_num_rows($users_online_query);
+            }
+        }
     }
+
+    user_online();
 
 ?>
