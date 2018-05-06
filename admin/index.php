53e151cab55c1090ebc44334fa0a459ae4dec4c3
LalitKhatri <kartikrobin@gmail.com>
2018-05-06 15:05:28 +0530

    created four variables and a table in DATABASE for track users online feature

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,26 @@
 <?php include("includes/header.php"); ?>
 
     <div id="wrapper">
+    
+<?php
+
+$session = session_id();
+$time = time();
+$time_out_in_seconds = 60;
+$time_out = $time - $time_out_in_seconds;
+
+$query = "SELECT * FROM users_online WHERE session = '$session'";
+$send_query = mysqli_query($connection, $query);
+
+$count = mysqli_num_rows($send_query);
+
+if($count == NULL)
+{
+    mysqli_query($connection, "INSERT INTO users_online(session, time) VALUES('$session', '$time')");
+}
+
+
+?>
 
         <!-- Navigation -->
         <?php include("includes/navigation.php"); ?>
