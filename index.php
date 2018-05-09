bb28a668fb1ddea7815176e8d6ded0c95461463d
LalitKhatri <kartikrobin@gmail.com>
2018-05-09 16:00:15 +0530

    used post_user instead post_author

--- index.php
+++ index.php
@@ -73,7 +73,7 @@
                     <a href="post.php?p_id=<?php echo $post_id;?>"><?php echo $post_title; ?></a>
                 </h2>
                 <p class="lead">
-                    by <a href="author_posts.php?author=<?php echo $post_author; ?>&p_id=<?php echo $post_id;?>"><?php echo $post_author; ?></a>
+                    by <a href="author_posts.php?author=<?php echo $post_user; ?>&p_id=<?php echo $post_id;?>"><?php echo $post_user; ?></a>
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date; ?></p>
                 <hr>
