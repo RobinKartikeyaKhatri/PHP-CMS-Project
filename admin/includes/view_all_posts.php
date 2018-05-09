88f82a6423c8033c703f8666d9c2d5be27dff77f
LalitKhatri <kartikrobin@gmail.com>
2018-05-09 15:15:38 +0530

    changed th to post_user, changed if condition also

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -88,7 +88,7 @@ if(isset($_POST['checkBoxArray']))
             <tr>
                 <th><input type="checkbox" name="" id="selectAllBoxes"></th>
                 <th>ID</th>
-                <th>Author</th>
+                <th>User</th>
                 <th>Title</th>
                 <th>Category</th>
                 <th>Status</th>
@@ -130,11 +130,11 @@ if(isset($_POST['checkBoxArray']))
         <?php
                 echo "<td>$post_id</td>";
 
-                if(isset($post_author) || !empty($post_author))
+                if(!empty($post_author))
                 {
                     echo "<td>$post_author</td>";    
                 }
-                elseif(isset($post_user) || !empty($post_user))
+                elseif(!empty($post_user))
                 {
                     echo "<td>$post_user</td>";
                 }
