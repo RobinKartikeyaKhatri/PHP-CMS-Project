f1d01747af50207872f960dfba998e367ea5564e
robinkartikeyakhatri <kartikrobin@gmail.com>
2018-05-13 13:38:50 +0530

    created code for modal

---
+++ admin/includes/delete_modal.php
@@ -0,0 +1,24 @@
+<!-- Trigger the modal with a button -->
+<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Modal Delete</h4>
+      </div>
+      <div class="modal-body">
+        <p>Are you sure you want to delete this post?</p>
+      </div>
+      <div class="modal-footer">
+        <a href="" class="btn btn-danger modal_delete_link">Delete</a>
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+      </div>
+    </div>
+
+  </div>
+</div>
