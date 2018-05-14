0ca6b3caf7169024b3c91214745fcec2686d1251
robinkartikeyakhatri <kartikrobin@gmail.com>
2018-05-14 08:27:04 +0530

    removed duplicated category display from edit_post.php page

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -88,7 +88,16 @@
                     $cat_id = $row['cat_id'];
                     $cat_title = $row['cat_title'];
                     
-                    echo "<option value='$cat_id'>$cat_title</option>";
+                
+
+                    if($cat_id == $post_category_id)
+                    {
+                        echo "<option selected value='$cat_id'>$cat_title</option>";
+                    }
+                    else
+                    {
+                        echo "<option value='$cat_id'>$cat_title</option>";
+                    }
                 }
             
             ?>
