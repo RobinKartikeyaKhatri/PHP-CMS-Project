bf5eadcf385214310248b2a3d81eb0e76013ec81
robinkartikeyakhatri <kartikrobin@gmail.com>
2018-05-14 10:12:39 +0530

    used recordCount() function to display char data

--- admin/index.php
+++ admin/index.php
@@ -32,18 +32,7 @@
                                         <i class="fa fa-file-text fa-5x"></i>
                                     </div>
                                     <div class="col-xs-9 text-right">
-
-                                    <?php
-                                    
-                                        $query = "SELECT * FROM posts";
-                                        $select_all_posts = mysqli_query($connection, $query);
-                                        $post_counts = mysqli_num_rows($select_all_posts);
-
-                                        echo "<div class='huge'>$post_counts</div>";
-                                    
-                                    ?>
-
-                                
+                                        <div class='huge'><?php echo $post_counts = recordCount('posts'); ?></div>
                                         <div>Posts</div>
                                     </div>
                                 </div>
@@ -65,17 +54,7 @@
                                         <i class="fa fa-comments fa-5x"></i>
                                     </div>
                                     <div class="col-xs-9 text-right">
-
-                                    <?php
-                                    
-                                        $query = "SELECT * FROM comments";
-                                        $select_all_comments = mysqli_query($connection, $query);
-                                        $comments_counts = mysqli_num_rows($select_all_comments);
-
-                                        echo "<div class='huge'>$comments_counts</div>";
-                                    
-                                    ?>
-
+                                        <div class='huge'><?php echo $comments_counts = recordCount('comments'); ?></div>
                                     <div>Comments</div>
                                     </div>
                                 </div>
@@ -97,17 +76,7 @@
                                         <i class="fa fa-user fa-5x"></i>
                                     </div>
                                     <div class="col-xs-9 text-right">
-
-                                    <?php
-                                    
-                                        $query = "SELECT * FROM users";
-                                        $select_all_users = mysqli_query($connection, $query);
-                                        $users_counts = mysqli_num_rows($select_all_users);
-
-                                        echo "<div class='huge'>$users_counts</div>";
-                                    
-                                    ?>
-
+                                        <div class='huge'><?php echo $users_counts = recordCount('users'); ?></div>
                                         <div> Users</div>
                                     </div>
                                 </div>
@@ -129,17 +98,7 @@
                                         <i class="fa fa-list fa-5x"></i>
                                     </div>
                                     <div class="col-xs-9 text-right">
-
-                                    <?php
-
-                                        $query = "SELECT * FROM categories";
-                                        $select_all_categories = mysqli_query($connection, $query);
-                                        $categories_counts = mysqli_num_rows($select_all_categories);
-
-                                        echo "<div class='huge'>$categories_counts</div>";
-                                    
-                                    ?>
-
+                                        <div class='huge'><?php echo $categories_counts = recordCount('categories'); ?></div>
                                         <div>Categories</div>
                                     </div>
                                 </div>
