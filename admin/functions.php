f4b1ccf27069215038a65c97df5dce0894514c5e
robinkartikeyakhatri <kartikrobin@gmail.com>
2018-05-15 09:17:25 +0530

    created is_admin() function to check the user is admin or not

--- admin/functions.php
+++ admin/functions.php
@@ -148,4 +148,26 @@
 
     }
 
+    function is_admin($username = "")
+    {
+        global $connection;
+
+        $query = "SELECT user_role FROM users WHERE username = '$username'";
+
+        $result = mysqli_query($connection, $query);
+
+        confirmQuery($result);
+
+        $row = mysqli_fetch_array($result);
+
+        if($row['user_role'] == 'Admin')
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
 ?>
