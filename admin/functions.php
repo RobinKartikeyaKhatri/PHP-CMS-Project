6eb51777c33bbdfad17f29079312a62b41aefcec
robinkartikeyakhatri <kartikrobin@gmail.com>
2018-05-15 11:17:58 +0530

    create a function to check duplicate username exists or not

--- admin/functions.php
+++ admin/functions.php
@@ -170,4 +170,24 @@
         }
     }
 
+    //Duplicate Username function
+    function username_exists($username)
+    {
+        global $connection;
+
+        $query = "SELECT username FROM users WHERE username = '$username'";
+        $result = mysqli_query($connection, $query);
+
+        confirmQuery($result);
+
+        if(mysqli_num_rows($result) > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
 ?>
